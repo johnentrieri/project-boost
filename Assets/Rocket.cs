07bb50e9484470e28f7c417343ed381eb90ce1c3
John Entrieri <john.entrieri@gmail.com>
2020-11-30 11:40:47 -0500

    Completed Lecture 51

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,41 +7,61 @@ public class Rocket : MonoBehaviour
     private Rigidbody rigidBody;
     private AudioSource audioSource;
 
-    private float upThrust = 1.5f;
-    private float rotThrust = 0.3f;
+    [SerializeField] float upThrust = 3.0f;
+    [SerializeField] float rotThrust = 100.0f;
 
     // Start is called before the first frame update
     void Start() {
         rigidBody = GetComponent<Rigidbody>();
+        rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+
         audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(upThrust * Vector3.up);
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                break;
+            default:
+                Destroy(gameObject);
+                break;
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(upThrust * Vector3.up);
-            audioSource.Play();
-        }
+    private void Rotate() {
 
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(upThrust * Vector3.up);
-            audioSource.Pause();
-        }
+        rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+        float frameRotation = rotThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(rotThrust * Vector3.forward);
+            transform.Rotate(frameRotation * Vector3.forward);
         }
 
         if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(rotThrust * Vector3.back);
+            transform.Rotate(frameRotation * Vector3.back);
         }
+
+        rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+    }
+
+    private void Thrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+                rigidBody.AddRelativeForce(upThrust * Vector3.up);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                audioSource.Play();
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space)) {
+                audioSource.Pause();
+            }
     }
 }
